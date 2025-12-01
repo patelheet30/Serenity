bfb832eef658fc143464f1eeb43286a01aad6da9
Heet Patel <heetkpatel30@gmail.com>
2025-12-01 01:41:33 +0000

    feat: initialise project structure with environment configuration, logging, and bot setup

---
+++ README.md
@@ -0,0 +1,5 @@
+A rewrite of Serenity bot in Python. Serenity handles slowmode automatically for Discord servers by monitoring message activity and adjusting slowmode settings accordingly. However, it was designed by me initially for a private single server with no consideration for future growth or development. This project aims to rewrite Serenity with scalability, maintainability, and extensibility in mind, allowing it to continue supporting multiple servers effectively, while also providing a solid foundation for future features and improvements.
+
+It uses hikari and hikari-arc for Discord API interactions, and aiosqlite for database management.
+
+The rewrite is still in its early stages, and many features from the original Serenity bot are yet to be implemented. Therefore, no contributions are being accepted at this time.
--- main.py
+++ main.py
@@ -1,6 +1,51 @@
-def main():
-    print("Hello from serenity!")
+import asyncio
+import logging
+import os
+from pathlib import Path
+
+import arc
+import hikari
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logging.basicConfig(
+    level=os.getenv("LOG_LEVEL", "INFO"),
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+)
+
+logger = logging.getLogger("serenity")
+
+if os.name != "nt":
+    try:
+        import uvloop
+
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+        logger.info("Using uvloop for improved performance.")
+    except ImportError:
+        logger.info("uvloop is not installed; using default asyncio event loop.")
+
+bot = hikari.GatewayBot(
+    token=os.environ["TOKEN"],
+    intents=hikari.Intents.GUILD_MESSAGES | hikari.Intents.GUILDS,
+)
+
+client = arc.GatewayClient(bot)
+
+
+@client.add_startup_hook
+async def on_startup(_: arc.GatewayClient) -> None:
+    """Called when the bot starts up."""
+    logger.info(f"Serenity is starting up... {bot.get_me()}")
+    logger.info("Bot if ready!")
+
+
+@client.add_shutdown_hook
+async def on_shutdown(_: arc.GatewayClient) -> None:
+    """Called when the bot is shutting down."""
+    logger.info("Serenity is shutting down...")
 
 
 if __name__ == "__main__":
-    main()
+    logger.info("Starting Serenity bot...")
+    bot.run()
